10409a229172a7f22eb8cfe965ed1ccd4f5a23eb
NullFluxKevin <null_flux_kevin@outlook.com>
2025-06-26 16:25:10 -0400

    feat: top title border box with left, center and right alignment

--- tui/box.nim
+++ tui/box.nim
@@ -4,6 +4,9 @@ import terminal
 
 import layoutEngine
 
+# TODO: Bottom Title border
+# TODO: Vertical Title borders
+
 template withCursorPos(x, y: int, body: untyped) =
   let (prevX, prevY) = getCursorPos()
   setCursorPos(x, y)
@@ -16,8 +19,10 @@ type
     borderLeft, canvas, borderRight: Rect
     cornerBottomLeft, borderBottom, cornerBottomRight: Rect
 
-  BoxSymbols = object
+  BorderSymbols = object
     horizontal, vertical, cornerTopLeft, cornerTopRight, cornerBottomLeft, cornerBottomRight: string
+
+  TitleAlignment= enum taLowerEnd, taCenter, taHigherEnd
 
 proc initBox(rect: Rect): Box = 
   doAssert rect.width >= 3
@@ -45,7 +50,7 @@ proc initBox(rect: Rect): Box =
   (result.cornerBottomLeft, result.borderBottom, result.cornerBottomRight) = (bottomRowCols[0], bottomRowCols[1], bottomRowCols[2])
 
 
-proc initBoxSymbols(horizontal, vertical, cornerTopLeft, cornerTopRight, cornerBottomLeft, cornerBottomRight: string): BoxSymbols = 
+proc initBorderSymbols(horizontal, vertical, cornerTopLeft, cornerTopRight, cornerBottomLeft, cornerBottomRight: string): BorderSymbols = 
   doAssert horizontal.runeLen == 1
   doAssert vertical.runeLen == 1
   doAssert cornerTopLeft.runeLen == 1
@@ -53,7 +58,7 @@ proc initBoxSymbols(horizontal, vertical, cornerTopLeft, cornerTopRight, cornerB
   doAssert cornerBottomLeft.runeLen == 1
   doAssert cornerBottomRight.runeLen == 1
 
-  BoxSymbols(
+  BorderSymbols(
     horizontal: horizontal,
     vertical: vertical,
     cornerTopLeft: cornerTopLeft,
@@ -62,35 +67,121 @@ proc initBoxSymbols(horizontal, vertical, cornerTopLeft, cornerTopRight, cornerB
     cornerBottomRight: cornerBottomRight,
   )
 
-const singleBoxSymbols = initBoxSymbols("─", "│", "┌", "┐", "└", "┘")
-const doubleBoxSymbols = initBoxSymbols("═", "║", "╔", "╗", "╚", "╝")
-const roundedBoxSymbols = initBoxSymbols("─", "│", "╭", "╮", "╰", "╯")
+const singleBorder = initBorderSymbols("─", "│", "┌", "┐", "└", "┘")
+const doubleBorder = initBorderSymbols("═", "║", "╔", "╗", "╚", "╝")
+const roundedBorder = initBorderSymbols("─", "│", "╭", "╮", "╰", "╯")
+const defaultBorder = roundedBorder
+
+proc defaultDraw(content: string) =
+  stdout.write(content)
+
+proc drawHorizontal(rect: Rect, content: string, draw: proc(content: string) = defaultDraw) = 
+  doAssert rect.height == 1
+  doAssert content.runeLen <= rect.width
+  withCursorPos(rect.x, rect.y):
+    draw(content)
+
+proc drawVertical(rect: Rect, content: string, draw: proc(content: string) = defaultDraw) = 
+  doAssert rect.width == 1
+  doAssert content.runeLen <= rect.height
+
+  for i in 0 ..< rect.height:
+    withCursorPos(rect.x, rect.y + i):
+      draw(content)
  
-proc drawBox(box: Box, symbols: BoxSymbols = roundedBoxSymbols) = 
-  withCursorPos(box.cornerTopLeft.x, box.cornerTopLeft.y):
-    stdout.write(symbols.cornerTopLeft)
-  withCursorPos(box.cornerTopRight.x, box.cornerTopRight.y):
-    stdout.write(symbols.cornerTopRight)
-  withCursorPos(box.cornerBottomLeft.x, box.cornerBottomLeft.y):
-    stdout.write(symbols.cornerBottomLeft)
-  withCursorPos(box.cornerBottomRight.x, box.cornerBottomRight.y):
-    stdout.write(symbols.cornerBottomRight)
+proc drawSymbol(rect: Rect, content: string, draw: proc(content: string) = defaultDraw) = 
+  doAssert rect.width == 1
+  doAssert rect.height == 1
+  doAssert content.runeLen == 1
+
+  withCursorPos(rect.x, rect.y):
+    draw(content)
 
 
+proc drawCorners(box: Box, symbols: BorderSymbols = defaultBorder) = 
+  drawSymbol(box.cornerTopLeft, symbols.cornerTopLeft)
+  drawSymbol(box.cornerTopRight, symbols.cornerTopRight)
+  drawSymbol(box.cornerBottomLeft, symbols.cornerBottomLeft)
+  drawSymbol(box.cornerBottomRight, symbols.cornerBottomRight)
+
+
+proc drawLeftBorder(box: Box, symbols: BorderSymbols = defaultBorder) = 
+  drawVertical(box.borderLeft, symbols.vertical)
+
+proc drawRightBorder(box: Box, symbols: BorderSymbols = defaultBorder) = 
+  drawVertical(box.borderRight, symbols.vertical)
+
+proc drawTopBorder(box: Box, symbols: BorderSymbols = defaultBorder) = 
   let horizontalBorder = symbols.horizontal.repeat(box.borderTop.width).join
+  drawHorizontal(box.borderTop, horizontalBorder)
 
-  withCursorPos(box.borderTop.x, box.borderTop.y):
-    stdout.write(horizontalBorder)
+proc drawBottomBorder(box: Box, symbols: BorderSymbols = defaultBorder) = 
+  let horizontalBorder = symbols.horizontal.repeat(box.borderTop.width).join
+  drawHorizontal(box.borderBottom, horizontalBorder)
+
+proc drawTitleBorderHorizontal(border: Rect, title: string, alignment: TitleAlignment = taLowerEnd, minLengthEachEnd:Natural = 2, minTitleSpacePadding:Natural = 1, symbols: BorderSymbols = defaultBorder) = 
+
+  let minSymbolLength = 2 * minLengthEachEnd + 2 * minTitleSpacePadding
+  doAssert title.runeLen <= border.width - minSymbolLength
+
+  let
+    padding = " ".repeat(minTitleSpacePadding)
+    paddedTitle = padding & title & padding
   
-  withCursorPos(box.borderBottom.x, box.borderBottom.y):
-    stdout.write(horizontalBorder)
+  var horizontalConstraints: seq[Constraint]
+  case alignment:
+  of taLowerEnd:
+    horizontalConstraints = @[
+      Constraint(kind: ckLength, length: minLengthEachEnd),
+      Constraint(kind: ckLength, length: paddedTitle.runeLen),
+      Constraint(kind: ckMinLength, minLength: minLengthEachEnd),
+    ]
 
-  for i in 0 ..< box.borderLeft.height:
-    withCursorPos(box.borderLeft.x, box.borderLeft.y + i):
-      stdout.write(symbols.vertical)
-    withCursorPos(box.borderRight.x, box.borderRight.y + i):
-      stdout.write(symbols.vertical)
-    
+  of taCenter:
+    let
+      available = border.width - title.runeLen - minSymbolLength
+      leftEndLength = available div 2
+
+    horizontalConstraints = @[
+      Constraint(kind: ckLength, length: leftEndLength + minLengthEachEnd),
+      Constraint(kind: ckLength, length: paddedTitle.runeLen),
+      Constraint(kind: ckMinLength, minLength: minLengthEachEnd),
+    ]
+
+  of taHigherEnd:
+    horizontalConstraints = @[
+      Constraint(kind: ckMinLength, minLength: minLengthEachEnd),
+      Constraint(kind: ckLength, length: paddedTitle.runeLen),
+      Constraint(kind: ckLength, length: minLengthEachEnd),
+    ]
+
+  let 
+    sections = layout(ldHorizontal, border, horizontalConstraints)
+    leftSection = sections[0]
+    titleSection = sections[1]
+    rightSection = sections[2]
+
+
+  drawHorizontal(leftSection, symbols.horizontal.repeat(leftSection.width))
+  drawHorizontal(titleSection, paddedTitle)
+  drawHorizontal(sections[2], symbols.horizontal.repeat(rightSection.width))
+
+
+proc drawTopTitleBox(box: Box, title: string, alignment: TitleAlignment = taLowerEnd) = 
+  box.drawCorners
+  box.borderTop.drawTitleBorderHorizontal(title, alignment)
+  box.drawBottomBorder
+  box.drawLeftBorder
+  box.drawRightBorder
+
+
+proc drawDefaultBox(box: Box) = 
+  box.drawCorners
+  box.drawTopBorder
+  box.drawBottomBorder
+  box.drawLeftBorder
+  box.drawRightBorder
+
     
 when isMainModule:
   try:
@@ -126,11 +217,17 @@ when isMainModule:
       rows = layout(ldVertical, main, rowsConstraints)
 
     # echo "Outer Box: ", rect
-    drawBox(outerBox, singleBoxSymbols)
-    for row in rows:
-      drawBox(initBox(row))
+    outerBox.drawTopTitleBox("Left Title")
 
-    drawBox(initBox(sidebar), doubleBoxSymbols)
+    for i, row in rows:
+      if i == 0:
+        initBox(row).drawTopTitleBox("Centered Title", alignment=taCenter)
+      elif i == 1:
+        initBox(row).drawTopTitleBox("Right Title", alignment=taHigherEnd)
+      else:
+        drawDefaultBox(initBox(row))
+
+    drawDefaultBox(initBox(sidebar))
  
 
     discard getch()
