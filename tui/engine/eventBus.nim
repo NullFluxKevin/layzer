a14eb6fa22eefab1517d47cee4ab74c0d32a5b50
NullFluxKevin <null_flux_kevin@outlook.com>
2025-07-17 14:20:59 -0400

    fix: Changed accessiblity of some eventBus module members to public

--- tui/engine/eventBus.nim
+++ tui/engine/eventBus.nim
@@ -1,21 +1,23 @@
 import terminal
 import posix
 import tables
+import options
 
 import tui/term/keyInput
 import tui/term/rawTerm
 
 
 type
-  EventContext = ref object of RootObj
+  EventContext* = ref object of RootObj
 
-  EventHandler = proc(ctx: EventContext)
-  Event = ref object
-    name: string
-    handlers: seq[EventHandler]
+  EventHandler* = proc(ctx: EventContext)
+  Event* = ref object
+    name*: string
+    handlers*: seq[EventHandler]
  
   EventBus = Table[string, Event]
-  EventQueue = Channel[tuple[e: Event, ctx: EventContext]]
+  EventInfo* = tuple[e: Event, ctx: EventContext]
+  EventQueue = Channel[EventInfo]
  
 
 var
@@ -23,20 +25,27 @@ var
   eventQueue = EventQueue()
 
 
-template withEventQueue(body: untyped) = 
+template withEventQueue*(body: untyped) = 
   try:
     eventQueue.open
     body
   finally:
     eventQueue.close
 
+proc tryGetEvent*(): Option[EventInfo] =
+  let recv = eventQueue.tryRecv()
+  if recv.dataAvailable:
+    result = some((recv.msg.e, recv.msg.ctx))
+  else:
+    result = none(EventInfo)
+    
 
-proc invoke(e: Event, ctx: EventContext) =
+proc invoke*(e: Event, ctx: EventContext) =
   for handler in e.handlers:
     handler(ctx)
 
 
-proc onEvent(event: string, handler: EventHandler) = 
+proc onEvent*(event: string, handler: EventHandler) = 
   # Table value gotcha:
   # if your value is of value type, then modification after assignment such as
   # ```nim
@@ -49,7 +58,7 @@ proc onEvent(event: string, handler: EventHandler) =
   e.handlers.add(handler)
 
 
-proc emitEvent(event: string, ctx: EventContext) = 
+proc emitEvent*(event: string, ctx: EventContext) = 
   doAssert event in eventBus, "No such event registered: " & event
 
   let toBeEmitted = eventBus[event]
