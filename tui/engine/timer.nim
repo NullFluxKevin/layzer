07a161d1db930383092d8078a977595addcf19f1
NullFluxKevin <null_flux_kevin@outlook.com>
2025-07-22 12:19:06 -0400

    feat: Runtime timer cancellation with limitations (read comment in demo section for details)

--- tui/engine/timer.nim
+++ tui/engine/timer.nim
@@ -2,6 +2,7 @@ import os
 import heapqueue
 import times
 import tables
+import sets
 
 
 type
@@ -18,11 +19,24 @@ type
   TimerEventQueue = Channel[TimerID]
   TimerHandler = proc()
 
+  TimerLoopCommandKind = enum
+    tlckCancel
+
+  TimerLoopCommand = object
+    case kind: TimerLoopCommandKind
+    of tlckCancel:
+      id: TimerID
+
+  CommandChannel = Channel[TimerLoopCommand]
+
 
 var
   timerEventQueue = TimerEventQueue()
   idCounter = 0
+  cmdCh = CommandChannel()
 
+timerEventQueue.open
+cmdCh.open
 
 const
   minIntervalAllowed = initDuration(milliseconds=1)
@@ -49,10 +63,15 @@ proc initTimer(interval: Duration, isOneShot: bool): Timer =
 proc registerTimer(registry: var TimerRegistry, timerID: TimerID, handler: TimerHandler) =
   registry[timerID] = handler
   
+proc cancelTimer(id: TimerID) = 
+  cmdCh.send(TimerLoopCommand(kind: tlckCancel, id: id))
+
 
 proc startTimers(timers: seq[Timer]) {.thread.} = 
 
-  var timerQueue = initHeapQueue[Timer]()
+  var
+    timerQueue = initHeapQueue[Timer]()
+    pendingCancellations = HashSet[TimerID]()
 
   for timer in timers:
     doAssert minIntervalAllowed <= timer.interval, "Fatal: Intervals smaller than 1ms are not allowed."
@@ -61,9 +80,13 @@ proc startTimers(timers: seq[Timer]) {.thread.} =
 
 
   while true:
-    let
-      nextTimer = timerQueue.pop()
-      timeTilActivation = nextTimer.due - getTime()
+    let nextTimer = timerQueue.pop()
+
+    if pendingCancellations.contains(nextTimer.id):
+      pendingCancellations.excl(nextTimer.id)
+      continue
+
+    let timeTilActivation = nextTimer.due - getTime()
   
     doAssert DurationZero < timeTilActivation, "Fatal: Timer due time is in the past. This may indicate host time drift or a bug in timer setup." &
     "\nTimer ID: " & $nextTimer.id & 
@@ -80,6 +103,16 @@ proc startTimers(timers: seq[Timer]) {.thread.} =
     if isTimerEventQueueClosed:
       return
 
+    while true:
+      let recv = cmdCh.tryRecv
+      if not recv.dataAvailable:
+        break
+
+      let cmd = recv.msg
+      case cmd.kind:
+      of tlckCancel:
+        pendingCancellations.incl(cmd.id)
+
     timerEventQueue.send(nextTimer.id)
 
     if not nextTimer.isOneShot:
@@ -90,26 +123,37 @@ proc startTimers(timers: seq[Timer]) {.thread.} =
 
 when isMainModule:
 
-  timerEventQueue.open
-
   proc tick1SecHandler() =
     echo "tick every 1 sec"
 
   proc tick2SecsHandler() =
     echo "tick every 2 secs"
 
+  proc toBeCancelledTimerHandler() =
+    doAssert false, "Fatal: The timer is supposed to be cancelled and never be triggered"
   
   var timerRegistry = TimerRegistry()
 
   let tick1sec = initTimer(initDuration(seconds=1), false)
   let tick2secs = initTimer(initDuration(seconds=2), false)
+  let toBeCancelledTimer = initTimer(initDuration(seconds=1, milliseconds=500), false)
 
   timerRegistry.registerTimer(tick1sec.id, tick1SecHandler)
   timerRegistry.registerTimer(tick2secs.id, tick2SecsHandler)
-  var timers: seq[Timer] = @[tick1sec, tick2Secs]
+  timerRegistry.registerTimer(toBeCancelledTimer.id, toBeCancelledTimerHandler)
+  var timers: seq[Timer] = @[tick1sec, tick2Secs, toBeCancelledTimer]
 
   var timerThread: Thread[seq[Timer]]
   createThread(timerThread, startTimers, timers)
+
+  # The timer thread must be started before sending control commands.
+  # Commands (like cancellation) are only processed once the timer loop is running.
+  # 
+  # IMPORTANT:
+  # The effect of sending a cancellation command before the loop starts is *not* guaranteed, unless the target timer's due time is far enough in the future to allow the cancellation to be processed *before* the timer is activated.
+  # 
+  # This behavior is by design: control commands are part of the running timer system, and cannot take effect before it begins.
+  cancelTimer(toBeCancelledTimer.id)
 
   var timerCounter = 0
   while true:
@@ -125,6 +169,8 @@ when isMainModule:
 
   # This is how we signal the timer thread to stop the loop
   timerEventQueue.close
+
+  cmdCh.close
 
   joinThread(timerThread)
 
