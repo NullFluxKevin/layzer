d19e4345bc8a1e0d916d14df8b734bf735f3717b
NullFluxKevin <null_flux_kevin@outlook.com>
2025-07-27 16:25:04 -0400

    feat: first draft of line-wise diff double buffering

--- tui/render/doubleBuffer.nim
+++ tui/render/doubleBuffer.nim
@@ -1,7 +1,10 @@
+import os
 import colors
 import terminal
 import strformat
-import unicode
+import sequtils
+import math
+import sets
 
 import layoutEngine
 
@@ -90,12 +93,134 @@ proc `$`(span: Span): string =
   result.add(ansiResetCode)
 
 
-when isMainModule:
-  let rect = initRect(0, 0, 20, 1)
+const stylePrefix = "\e["
 
-  let spanTrueColor = toSpan(rect, "True Color", initSpanColors(colPurple, colBurlyWood), {styleBlink, styleUnderscore})
-  let spanANSIColor= toSpan(rect, "ANSI Color", initSpanColors(fgYellow, bgBlack))
+type
+  Buffer = object
+    lines: seq[Rect]
+    front, back: seq[seq[Span]]
+    changedLines: Hashset[Natural]
 
-  echo $spanTrueColor
-  echo $spanANSIColor
+
+proc initBuffer(rect: Rect): Buffer =
+  var constraints: seq[Constraint] = @[]
+  for _ in 0..<rect.height:
+    constraints.add(Constraint(kind: ckLength, length: 1))
+    
+  result.lines = layout(ldVertical, rect, constraints)
+  result.front = newSeq[seq[Span]](rect.height)
+  result.back = newSeq[seq[Span]](rect.height)
+  result.changedLines = HashSet[Natural]()
+
+
+proc `[]`(buffer: Buffer, lineNumber: int): Rect = 
+  buffer.lines[lineNumber]
+
+
+proc width(buffer: Buffer): Natural =
+  buffer[0].width
+
+
+proc height(buffer: Buffer): Natural =
+  buffer[0].height
+
+
+proc onLine(buffer: var Buffer, lineNumber: int, spans: varargs[Span]) =
+  let widthSum = spans.map(proc(span: Span): Natural = span.rect.width).sum
+
+  doAssert widthSum <= buffer.width, "Error: Spans exceed line length"
+  buffer.back[lineNumber] = spans.toSeq
+  buffer.changedLines.incl(lineNumber)
+
+
+proc cursorPositionPrefix(x, y: Natural): string = 
+  fmt"{stylePrefix}{y+1};{x+1}f"
+
+
+proc addNewLineSuffix(s: var string) =
+  s &= "\r\n"
   
+
+proc dumpBuffer(buffer: Buffer, dumpBackBuffer: bool): string =
+  let toDump = if dumpBackBuffer: buffer.back else: buffer.front
+
+  for line in toDump:
+    if line.len == 0:
+      result.addNewLineSuffix()
+      continue
+
+    for span in line:
+      result.add($span)
+
+    result.addNewLineSuffix()
+
+
+proc renderFrame(buffer: var Buffer): string =
+  while buffer.changedLines.len > 0:
+
+    let
+      lineNumber = buffer.changedLines.pop
+      line = buffer[lineNumber]
+
+    result.add(cursorPositionPrefix(line.x, line.y))
+
+    for span in buffer.back[lineNumber]:
+      result.add($span)
+    
+    result.addNewLineSuffix()
+
+
+  (buffer.front, buffer.back) = (buffer.back, buffer.front)
+
+      
+when isMainModule:
+  let rect = initRect(10, 10, 30, 10)
+  
+  var buffer = initBuffer(rect)
+
+  let
+    line0 = buffer[0]
+    counterLine = buffer[1]
+    line2 = buffer[2]
+
+    span0Content = "True Color"
+    span1Content = "ANSI Color"
+    counterLabel = "Counted: "
+    counterSuffix = "time(s)"
+
+    line0Rects = layout(ldHorizontal, line0, @[
+      Constraint(kind: ckMinLength, minLength: span0Content.len),
+    ])
+
+    line2Rects = layout(ldHorizontal, line2, @[
+      Constraint(kind: ckMinLength, minLength: span1Content.len),
+    ])
+
+
+    counterLineRects = layout(ldHorizontal, counterLine, @[
+      Constraint(kind: ckLength, length: counterLabel.len),
+      Constraint(kind: ckMinLength, minLength: 0),
+      Constraint(kind: ckLength, length: counterSuffix.len),
+    ])
+
+    span0Rect = line0Rects[0]
+    span1Rect = line2Rects[0]
+
+  let spanTrueColor = toSpan(span0Rect, span0Content, initSpanColors(colPurple, colBurlyWood), {styleBlink, styleUnderscore})
+
+  let spanANSIColor = toSpan(span1Rect, span1Content, initSpanColors(fgYellow, bgBlack))
+
+  let counterLabelSpan = toSpan(counterLineRects[0], counterLabel, initSpanColors(fgDefault, bgDefault))
+
+  let counterSuffixSpan = toSpan(counterLineRects[2], counterSuffix, initSpanColors(fgDefault, bgDefault))
+
+  buffer.onLine(0, spanTrueColor)
+  buffer.onLine(2, spanANSIColor)
+
+  for i in 0 ..< 100000:
+    let counterText = fmt" {$i} "
+    let counterSpan = toSpan(counterLineRects[1], counterText, initSpanColors(fgDefault, bgDefault))
+    buffer.onLine(1, counterLabelSpan, counterSpan, counterSuffixSpan)
+    
+    echo buffer.renderFrame()
+    # sleep(10)
